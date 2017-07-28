224138bc31d352004bd15778fc0f3d8759669df0
Alex Avilov <alex.avilov@gmail.com>
2017-07-27 19:13:31 -0700

    initial commit

---
+++ class1/create_list.py
@@ -0,0 +1,12 @@
+def create_list():
+    my_list = range(6)
+    my_list.append("text")
+    my_list.append("more text")
+    my_list.append({})
+    my_list[-1]["one"] = 1
+    my_list[-1]["two"] = 2
+    my_list[-1][2] = 'two'
+    my_list[-1][1] = 'one'
+    my_list[-1]['digit'] = range(10)
+
+    return( my_list)
---
+++ class1/ex6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import yaml 
+import json
+import create_list
+
+my_list = create_list()
+print my_list
